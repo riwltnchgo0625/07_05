8b30cc2644bb8826a671779ac547eb5b8a216678
riwltnchgo0625 <63505725+riwltnchgo0625@users.noreply.github.com>
2022-07-05 14:46:48 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a22_summerapp_1/Users.java
@@ -0,0 +1,13 @@
+package com.example.a22_summerapp_1;
+
+public class Users {
+    private String image , name;
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
